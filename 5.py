e2b812d0baeb84268a661b3219d3af0617a17ae6
ivv1as <91069855+ivv1as@users.noreply.github.com>
2021-10-08 18:16:06 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -3,21 +3,25 @@ try:
     a = int(input()) 
 except ValueError: 
     print('Число должно быть целым и неотрицательным')
+    exit()
 print('Введите ширину первого прямоугольника')
 try: 
     b = int(input())
 except ValueError:
     print('Число должно быть целым и неотрицательным')
+    exit()
 print('Введите длину второго прямоугольника')
 try: 
     c = int(input())
 except ValueError:
     print('Число должно быть целым и неотрицательным')
+    exit()
 print('Введите длинну второго прямоугольника')
 try:
     d = int(input())
 except ValueError:
     print('Число должно быть целым и неотрицательным')
+    exit()
 if ((c-a)>=1 and (d-b)>=1) :print('Да')
 else: 
     if ((c-d)>=1 and (d-a)>=1) :print('Да')
